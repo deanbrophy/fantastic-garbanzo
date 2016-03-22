0947168e22b461c2eda8f4222adb0b943df43bd7
deanbrophy <dean.brophy@gmail.com>
2016-03-22 13:03:26 -0400

    clean up

--- web/elm/ElixirCast.elm
+++ web/elm/ElixirCast.elm
@@ -3,4 +3,4 @@ module ElixirCast where
 import Html
 
 main =
-  Html.text "Yeah, I'm excited! I wish it was easy, but Semantic's dependency on Gulp made it difficult. Migrating"
+  Html.text "Yeah, I'm excited! I wish it was easy, but Semantic's dependency on Gulp made it difficult. Clean up"
